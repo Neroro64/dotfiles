ddb5a698cba7c307a208073da7f8e49e8250f558
NuoC <nuoc@abyssalminds.com>
2024-04-28 20:53:45 +0200

    Added Hop.nvim plugin

--- configs/astrovim/lua/community.lua
+++ configs/astrovim/lua/community.lua
@@ -13,4 +13,5 @@ return {
   { import = "astrocommunity.editing-support.undotree" },
   { import = "astrocommunity.syntax.vim-easy-align" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.hop-nvim" },
 }
