3d05625adb561d9d72c0f67ffef6c6c05cb16af9
NuoC <nuoc@abyssalminds.com>
2024-04-30 23:21:43 +0200

    Added basic bookmarks

--- scripts/Backup-System.ps1
+++ scripts/Backup-System.ps1
@@ -1,14 +1,21 @@
+param(
+  [switch] $SyncToRemoteRepo
+)
 . /home/nuoc/.config/powershell/scripts/Sync-Backup.ps1
 . /home/nuoc/.config/powershell/scripts/New-rdiffBackup.ps1
 . /home/nuoc/.config/powershell/scripts/Mount-SSHFS.ps1
 
-Sync-Backup -SourceDir:"/" -DestinationDir /mnt/f/Backup/sys.bak/ -FilterPath /home/nuoc/.dotfiles/scripts/backup.filter
+Sync-Backup -SourceDir:"/" -DestinationDir /mnt/e/Backup/sys.bak/ -FilterPath /home/nuoc/.dotfiles/scripts/backup.filter
 New-rdiffBackup -SourceDir /mnt/f/Backup/sys.bak/ -DestinationDir /mnt/f/Backup/archive/
 
-try {
-# Sync to remote drive if available
+if ($SyncToRemoteRepo) {
+  try {
+  # Sync to remote drive if available
 
-Mount-SSHFS -ConfigName honeypot -RemoteDir /home/homie/Lake/Vanern -MountPoint /home/nuoc/Network/HoneyPot.Lake.Vanern
-Sync-Backup -SourceDir:"/" -DestinationDir /home/nuoc/Network/HoneyPot.Lake.Vanern/Backups/7950x -FilterPath /home/nuoc/.dotfiles/scripts/backup.filter -Options "--archive", "--verbose", "--delete", "--progress", "-H", "--no-o", "--no-g"
+  Mount-SSHFS -ConfigName honeypot -RemoteDir /home/homie/Lake/Vanern -MountPoint /home/nuoc/Network/HoneyPot.Lake.Vanern
+  Sync-Backup -SourceDir:"/" -DestinationDir /home/nuoc/Network/HoneyPot.Lake.Vanern/Backups/7950x -FilterPath /home/nuoc/.dotfiles/scripts/backup.filter -Options "--archive", "--verbose", "--delete", "--progress", "-H", "--no-o", "--no-g"
+  }
+  catch {
+    Write-Error "Failed to sync backup to remote repo. Error: $_"
+  }
 }
-catch {}
