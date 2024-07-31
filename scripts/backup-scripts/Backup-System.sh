9ff13966cb86b565d5e6a27560436b1c7efeeabe
NuoC <nuoc@abyssalminds.com>
2024-07-31 20:18:46 +0200

    backup: added sync-backup service that syncs the local backups to remote location

--- scripts/backup-scripts/Backup-System.sh
+++ scripts/backup-scripts/Backup-System.sh
@@ -35,4 +35,4 @@ fi
 restic backup --exclude-file "$ExcludeFilePath" --files-from "$IncludeFilePath" --repo "$RepositoryName"
 
 # Clean old snapshots
-restic forget --keep-last "$MaxSnapshotCount" --prune
+restic forget --keep-last "$MaxSnapshotCount" --prune --repo "$RepositoryName"
