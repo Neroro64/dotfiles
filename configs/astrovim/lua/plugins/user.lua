a1871cdda50eac6459b3c1d679d47e00cdf6f25b
Chen, Nuo <nuchen@ea.com>
2025-03-19 13:09:42 +0100

    nvim: Removed unneeded plugins

--- configs/astrovim/lua/plugins/user.lua
+++ configs/astrovim/lua/plugins/user.lua
@@ -32,37 +32,22 @@ return {
       )
     end,
   },
-  {
-    "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "astronvim/astrocore",
-      opts = {
-        diagnostics = {
-          -- Disable diagnostics virtual text to prevent duplicates
-          virtual_text = false,
-        },
-      },
-    },
-    opts = {},
-  },
-  {
-    "theHamsta/nvim-dap-virtual-text",
-    opts = {
-      virt_text_pos = "eol",
-    },
-  },
-  {
-    "loctvl842/monokai-pro.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require("monokai-pro").setup {
-        filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-      }
-      vim.cmd.colorscheme "monokai-pro"
-    end,
-  },
   { "modularml/mojo.vim" },
   { "pteroctopus/faster.nvim" },
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    -- dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    config = function()
+        vim.g.zenbones_darken_comments = 45
+        vim.g.zenbones_compat = 1
+        vim.cmd.colorscheme('zenbones')
+        vim.opt.background = "light" -- dark or light
+    end
+  }
 }
