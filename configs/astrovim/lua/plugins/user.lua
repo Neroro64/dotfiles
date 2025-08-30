cc992f3533bee4863bce51c662ce6d5d508cea47
NuoC <me@nuoc.dev>
2025-08-30 16:56:46 +0200

    nvim: Added Evergarden colorscheme

--- configs/astrovim/lua/plugins/user.lua
+++ configs/astrovim/lua/plugins/user.lua
@@ -34,6 +34,25 @@ return {
   },
   { "pteroctopus/faster.nvim" },
   { "echasnovski/mini.align", config = function() require("mini.align").setup() end },
-  { "datsfilipe/vesper.nvim" },
-  { "thesimonho/kanagawa-paper.nvim" },
+  { "thesimonho/kanagawa-paper.nvim", lazy = true },
+  { 'everviolet/nvim', name = 'evergarden',
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    opts = {
+      theme = {
+        variant = 'winter', -- 'winter'|'fall'|'spring'|'summer'
+        accent = 'green',
+      },
+      editor = {
+        transparent_background = false,
+        sign = { color = 'none' },
+        float = {
+          color = 'mantle',
+          solid_border = false,
+        },
+        completion = {
+          color = 'surface0',
+        },
+      },
+    }
+  }
 }
