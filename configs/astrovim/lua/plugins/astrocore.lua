f8ba5b261207c1b4d0005b7678fb128d6319f1ca
NuoC <me@nuoc.dev>
2025-11-01 01:13:33 +0100

    nvim: set wrap=true

--- configs/astrovim/lua/plugins/astrocore.lua
+++ configs/astrovim/lua/plugins/astrocore.lua
@@ -24,7 +24,7 @@ return {
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true, -- sets vim.opt.wrap
         clipboard = "", -- resets the clipboard integration
         fixendofline = false,
         laststatus = 3,
