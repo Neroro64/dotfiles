4140975e38db19ddc44deb1d0bcd52b3c223649b
NuoC <me@nuoc.dev>
2026-01-16 00:44:15 +0100

    nvim: Added mojo indent setting

--- configs/lazynvim/lua/config/autocmds.lua
+++ configs/lazynvim/lua/config/autocmds.lua
@@ -5,3 +5,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "mojo",
+  callback = function()
+    -- Use 4 spaces for indentation in Mojo files
+    vim.opt_local.tabstop = 4 -- A tab counts as 4 spaces
+    vim.opt_local.shiftwidth = 4 -- Number of spaces for indentation
+    vim.opt_local.expandtab = true -- Convert tabs to spaces
+    vim.opt_local.softtabstop = 4 -- Number of spaces for tab key
+  end,
+})
