1683377b764b8845ffaa8c0fb72edf7e200d3ebd
NuoC <me@nuoc.dev>
2026-01-22 13:24:05 +0100

    nvim: updated sidekick toggle keymap

--- configs/lazynvim/lua/plugins/sidekick.lua
+++ configs/lazynvim/lua/plugins/sidekick.lua
@@ -6,4 +6,14 @@ return {
       enabled = false,
     },
   },
+  keys = {
+    {
+      "<M-.>",
+      function()
+        require("sidekick.cli").toggle()
+      end,
+      desc = "Sidekick Toggle",
+      mode = { "n", "t", "i", "x" },
+    },
+  },
 }
