7b8812f8d3d00b3bb7563727801cb3c34439241f
NuoC <me@nuoc.dev>
2026-03-29 16:07:33 +0200

    nvim: mojo-lsp config fix

--- configs/lazynvim/lua/plugins/mojo-support.lua
+++ configs/lazynvim/lua/plugins/mojo-support.lua
@@ -1,28 +1,30 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        mojo = {
-          cmd = { "mojo-lsp-server", "-I", "src" },
-          filetypes = { "mojo" },
-        },
-      },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        mojo = { "mojo" },
-      },
-      formatters = {
-        mojo = {
-          command = "mojo",
-          args = { "format", "-" },
-          stdin = true,
-        },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      mojo = {
+        cmd = { "mojo-lsp-server", "-I", "src" },
+        filetypes = { "mojo" },
+        on_exit = function(code, signal, client_id)
+          if signal ~= 0 then
+            vim.notify(
+              string.format(
+                "Mojo LSP crashed (code: %s, signal: %s). Automatically restarting...",
+                tostring(code),
+                tostring(signal)
+              ),
+              vim.log.levels.WARN,
+              { title = "LSP" }
+            )
+            vim.defer_fn(function()
+              local client = vim.lsp.get_client_by_id(client_id)
+              if client then
+                client:stop()
+              end
+              vim.api.nvim_exec_autocmds("FileType", { modeline = false })
+            end, 100)
+          end
+        end,
       },
     },
   },
